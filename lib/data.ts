f5db031e6df32afefc2b53f19b5df02489bbcbbc
invita-sin <sinyo.freelancer@gmail.com>
2026-06-13 08:24:28 +0700

    update

--- lib/data.ts
+++ lib/data.ts
@@ -6,7 +6,7 @@ export const weddingData = {
     fullGroom: "Rafiq Ahmad",
     parentsBride: "Bapak H. Suharto & Ibu Hj. Siti Fatimah",
     parentsGroom: "Bapak Drs. H. Maulana & Ibu Hj. Dewi Sartika",
-    photoCouple: "/public/images/couple.jpg",
+    photoCouple: "/images/couple.jpg",
   },
   date: {
     full: "Sabtu, 26 Desember 2026",
